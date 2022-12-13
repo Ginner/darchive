a12dfc5af982f23a9304752e7f45d9c655515692
Ginner <26798615+Ginner@users.noreply.github.com>
2022-12-13 06:36:39 +0100

    Handle spaces in files

--- darchive.sh
+++ darchive.sh
@@ -6,7 +6,7 @@
 # folder for indefinite storage
 # By ***REMOVED***
 #
-# Last modified: 2022.12.13-06:34 +0100
+# Last modified: 2022.12.13-06:36 +0100
 #
 # =============================================================== #
 
@@ -134,11 +134,11 @@ for d in "$dir"/*/; do
         # Consider using rsync for moving files, it might not be as omnipresent though...
         # That would make it posssible to make a backup dir
         if [[ "$silent" == "true" ]]; then
-            /usr/bin/mv --backup=numbered --force --target-directory="$targetdir" "$file" >/dev/null
+            /usr/bin/mv --backup=numbered --force --target-directory="$targetdir" \""$file"\" >/dev/null
         elif [[ "$verbose" == "true" ]]; then
-            /usr/bin/mv --backup=numbered --force --verbose --target-directory="$targetdir" "$file"
+            /usr/bin/mv --backup=numbered --force --verbose --target-directory="$targetdir" \""$file"\"
         else
-            /usr/bin/mv --backup=numbered --force --target-directory="$targetdir" "$file"
+            /usr/bin/mv --backup=numbered --force --target-directory="$targetdir" \""$file"\"
         fi
     done
 done
